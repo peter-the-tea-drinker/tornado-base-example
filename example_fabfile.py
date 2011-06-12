d393a2445cbcadd1b7a71f3b4a46486e6a023fa3
Peter Row <peter.row@gmail.com>
2011-06-13 02:59:27 +1000

    restructure

--- example_fabfile.py
+++ example_fabfile.py
@@ -1,9 +1,17 @@
-from fabbase import *
+import os,sys
+sys.path.append(os.path.expanduser('~/repos/tornado-base'))
+import base.fabbase
 
-CODE_DIR = '~/example.com'
-BASE_REPO = 'git@github.com:peter-the-tea-drinker/tornado-base.git'
-TAG = ''
+base.fabbase.APP_REPO = 'git://github.com/peter-the-tea-drinker/tornado-base-example.git'
+base.fabbase.APP_TAG = ''
 
-update_settings(code_dir=CODE_DIR,repo=REPO,release_tag=TAG)
+base.fabbase.STAGE_APP_DIR = '~/stage.example.com'
+base.fabbase.PROD_APP_DIR = '~/prod.eexample.com'
 
+base.fabbase.BASE_DIR = '~/tornado-base'
+base.fabbase.BASE_REPO = 'git://github.com/peter-the-tea-drinker/tornado-base.git'
+base.fabbase.BASE_TAG = ''
+
+stage = base.fabbase.stage
+prod = base.fabbase.prod
 
--- example_passenger_wsgi.py
+++
@@ -1,2 +0,0 @@
-from base.passenger_wsgi_base import *
-
